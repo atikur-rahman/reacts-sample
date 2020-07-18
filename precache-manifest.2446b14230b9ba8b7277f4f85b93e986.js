16485e90a2f3c4ea92285a6818535648d597cc32
Atikur Rahman <atikur.rahman96@yahoo.com.au>
2020-07-19 00:54:38 +1000

    Updates

---
+++ precache-manifest.2446b14230b9ba8b7277f4f85b93e986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7de098b69b55d97f1ddd709e3772cc7",
+    "url": "/reacts-sample/index.html"
+  },
+  {
+    "revision": "5dd76f7b5723234e0d57",
+    "url": "/reacts-sample/static/css/main.48ea4baa.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/reacts-sample/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacts-sample/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd76f7b5723234e0d57",
+    "url": "/reacts-sample/static/js/main.a3fa3287.chunk.js"
+  },
+  {
+    "revision": "504fe4374339890995cb",
+    "url": "/reacts-sample/static/js/runtime-main.50a27a1b.js"
+  }
+]);
